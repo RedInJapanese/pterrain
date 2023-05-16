e836635a708e45d1bc609492b5308349974a7534
RedInJapanese <abasu924@gmail.com>
2023-05-15 22:23:04 -0700

    update: script.js

--- src/script.js
+++ src/script.js
@@ -261,15 +261,51 @@ controller2.addEventListener( 'connected', ( event )=> {
 controller2.addEventListener( 'selectstart', ( event )=> {
     controller2.gamepad = event.data.gamepad
     console.log(controller2.gamepad.buttons)
+    if ( controller2.gamepad.buttons[0].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[1].pressed == true){
+        controller2.add( line);
+    }
     if ( controller2.gamepad.buttons[2].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[3].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[4].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[5].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[6].pressed == true){
         controller2.add( line);
     }
 });
 controller2.addEventListener( 'selectend', ( event )=> {
     controller2.gamepad = event.data.gamepad
     console.log(controller2.gamepad.buttons)
+    if ( controller2.gamepad.buttons[0].pressed == false){
+        controller2.remove( line);
+    }
+    if ( controller2.gamepad.buttons[1].pressed == false){
+        controller2.remove( line);
+    }
     if ( controller2.gamepad.buttons[2].pressed == false){
-        controller2.remove(line);
+        controller2.remove( line);
+    }
+    if ( controller2.gamepad.buttons[3].pressed == false){
+        controller2.remove( line);
+    }
+    if ( controller2.gamepad.buttons[4].pressed == false){
+        controller2.remove( line);
+    }
+    if ( controller2.gamepad.buttons[5].pressed == false){
+        controller2.remove( line);
+    }
+    if ( controller2.gamepad.buttons[6].pressed == false){
+        controller2.remove( line);
     }
 });
 
