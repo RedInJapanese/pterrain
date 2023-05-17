ee613cc779a7b3f1450ba166ed5c3c8bffa54222
RedInJapanese <abasu924@gmail.com>
2023-05-17 16:23:18 -0700

    update: script.js

--- src/script.js
+++ src/script.js
@@ -251,31 +251,31 @@ function line_appear(event){
 //     // console.log(event.data.gamepad.buttons[6].pressed)
 // });
 
-// controller2.addEventListener( 'connected', ( event )=> {
-//     controller2.gamepad = event.data.gamepad
-//     console.log(controller2.gamepad.buttons)
-//     if ( controller2.gamepad.buttons[0].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[1].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[2].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[3].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[4].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[5].pressed == true){
-//         controller2.add( line);
-//     }
-//     if ( controller2.gamepad.buttons[6].pressed == true){
-//         controller2.add( line);
-//     }
-// });
+controller2.addEventListener( 'connected', ( event )=> {
+    controller2.gamepad = event.data.gamepad
+    console.log(controller2.gamepad.buttons)
+    if ( controller2.gamepad.buttons[0].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[1].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[2].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[3].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[4].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[5].pressed == true){
+        controller2.add( line);
+    }
+    if ( controller2.gamepad.buttons[6].pressed == true){
+        controller2.add( line);
+    }
+});
 // controller2.addEventListener( 'selectstart', ( event )=> {
 //     controller2.gamepad = event.data.gamepad
 //     console.log(controller2.gamepad.buttons)
